2508baeab7befd61d460f211929d834146d07e09
karthik96siddhu <karthik96siddhu@gmail.com>
2026-07-12 22:22:47 +0530

    Add test_db_url as optional

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
 
 class Settings(BaseSettings):
     # Define your variables and their types
     DATABASE_URL: str
+    TEST_DATABASE_URL: Optional[str] = None
     SECRET_KEY: str
     DEBUG: bool = False  # Default value if not provided
 
     # Tell pydantic to read from .env file
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8', extra='ignore')
 
 # Instantiate the settings to be used accross the app
 settings = Settings()
